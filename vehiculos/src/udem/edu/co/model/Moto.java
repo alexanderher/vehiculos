8a239440ebed63cdaa961e7eb4acb04b0043e223
ahernanl <Eugenio.Uribe@TigoUne.com>
2020-09-23 06:35:15 -0500

    codigo

---
+++ vehiculos/src/udem/edu/co/model/Moto.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udem.edu.co.model;
+
+/**
+ *
+ * @author INDRA
+ */
+public class Moto {
+
+    private String placa;
+    private String marca;    
+    
+    public Moto() {
+    }
+    
+    ///getters and setters
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto{" + "placa=" + placa + ", marca=" + marca + ", peaje= 0 '}'";
+    }
+    
+    
+    
+}
